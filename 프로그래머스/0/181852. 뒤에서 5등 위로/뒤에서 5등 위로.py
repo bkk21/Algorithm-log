af8956b845754b59e33ee94048e44d051e6a8e30
Booooo Gyeong <108513540+bkk21@users.noreply.github.com>
2024-03-23 15:40:06 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.py
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.py
@@ -1,18 +1,26 @@
-def quick(array):
-
-    if len(array) <= 1:
-        return array
-
-    pivot = array[0]
-    not_pivot = array[1:]
-
-    left = [x for x in not_pivot if x <= pivot]
-    right = [x for x in not_pivot if x > pivot]
-
-    return quick(left) + [pivot] + quick(right)
-        
+#이게 좀 더 간결한 방법
 def solution(num_list):
     answer = []
 
-    answer = quick(num_list)[5:]
+    num_list.sort()
+    answer = num_list[5:]
     return answer
+
+# def quick(array):
+
+#     if len(array) <= 1:
+#         return array
+
+#     pivot = array[0]
+#     not_pivot = array[1:]
+
+#     left = [x for x in not_pivot if x <= pivot]
+#     right = [x for x in not_pivot if x > pivot]
+
+#     return quick(left) + [pivot] + quick(right)
+        
+# def solution(num_list):
+#     answer = []
+
+#     answer = quick(num_list)[5:]
+#     return answer
