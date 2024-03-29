6a64468ec26fb9327a0c9496a88bf012a7ce404a
Booooo Gyeong <108513540+bkk21@users.noreply.github.com>
2024-03-29 15:38:47 +0900

    [level 0] Title: 코드 처리하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,24 @@
+def solution(code):
+    ret = ''
+    
+    mode = 0
+        
+    for idx in range(len(code)):
+        if mode == 0:
+            if code[idx] == "1":
+                mode = 1
+            else:
+                if idx % 2 == 0 :
+                    ret += code[idx]
+                
+        else:
+            if code[idx] == "1":
+                mode = 0
+            else:
+                if idx % 2 == 1:
+                    ret += code[idx]
+    
+    if ret == "":
+        ret = "EMPTY"
+        
+    return ret
