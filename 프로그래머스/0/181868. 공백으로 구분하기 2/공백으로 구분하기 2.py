02cc251312b282e300fdfc61fe5228234b047dde
Booooo Gyeong <108513540+bkk21@users.noreply.github.com>
2025-03-10 22:27:56 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.py
@@ -0,0 +1,13 @@
+def solution(my_string):
+#     answer = my_string.split(' ')
+    
+    
+#     print(answer)
+    
+#     for i in answer:
+#         if not i.isalpha():
+#             answer.remove(i)
+            
+#     return answer
+
+    return my_string.split()
