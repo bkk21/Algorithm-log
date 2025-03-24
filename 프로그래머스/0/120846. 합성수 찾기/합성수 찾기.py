2d90c9fd94c6c26910961fe8a6bc7b9720030eed
Booooo Gyeong <108513540+bkk21@users.noreply.github.com>
2025-03-24 21:20:58 +0900

    [level 0] Title: 합성수 찾기, Time: 0.26 ms, Memory: 9.06 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,7 +1,10 @@
 def solution(n):
     answer = 0
     
+    #n까지 중에서 소수 개수 찾기
     for i in range(n+1):
         if len([x for x in range(1, i+1) if i % x == 0]) == 2:
             answer += 1
+    
+    #전체에서 소수의 개수를 빼면 합성수의 개수 (1도 제외해야 하기 때문에 -1)
     return n - answer - 1
