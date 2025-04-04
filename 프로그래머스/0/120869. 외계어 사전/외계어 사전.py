0dc88288096527a3944950cbc2516e14febcd6c4
Booooo Gyeong <108513540+bkk21@users.noreply.github.com>
2025-04-04 19:59:20 +0900

    [level 0] Title: 외계어 사전, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
@@ -2,7 +2,7 @@ def solution(spell, dic):
     answer = 2
     
     for i in dic:
-        if set(i) == set(spell):
+        if set(i) == set(spell) and len(i) == len(spell):
             answer = 1
             break
 
