a115ba1957fe6c006111b8b99ee8b5894dc98627
Booooo Gyeong <108513540+bkk21@users.noreply.github.com>
2025-04-06 19:05:46 +0900

    [level 0] Title: 등수 매기기, Time: 0.98 ms, Memory: 58.8 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
@@ -1,26 +1,16 @@
 import pandas as pd
 def solution(score):
-#     answer = []
-#     avg_dic = {}
-    
-#     for i in score:
-#         avg_dic[str(score.index(i) + 1)] = sum(i) / 2
-    
-#     print(avg_dic)
-#     sort_avg = sorted(avg_dic.values(), reverse=True)
-#     print(sort_avg)
-    
-#     for i in range(len(sort_avg)):
-#         if sort_avg.count(avg_dic[str(i + 1)]) >= 2:
-#             answer.append(sort_avg.index(avg_dic[str(i + 1)]) + sort_avg.count(avg_dic[str(i + 1)]) - 1)
-#         else:
-#             answer.append(sort_avg.index(avg_dic[str(i + 1)]) + 1)
     
     answer = []
+    
+    #평균 리스트 생성
     avg_list = []
+    
+    #평균 계산
     for i in score:
         avg_list.append(sum(i) / 2)
     
+    #데이터 프레임 활용
     avg_pd = pd.DataFrame({'avg':avg_list})
     answer = list(avg_pd['avg'].rank(method='min', ascending=False))
     
