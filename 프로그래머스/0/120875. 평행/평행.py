b5aa0eb27dfb27ab0661f98932e55635682d80e5
Booooo Gyeong <108513540+bkk21@users.noreply.github.com>
2025-04-09 21:13:58 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.py
+++ 프로그래머스/0/120875. 평행/평행.py
@@ -1,12 +1,17 @@
 def solution(dots):
+    #초기 설정
     answer = 0
+    
     #1번 case
     if (dots[1][0] - dots[0][0]) / (dots[1][1] - dots[0][1]) == (dots[3][0] - dots[2][0]) / (dots[3][1] - dots[2][1]):
         answer = 1
+        
+    #2번 case
     elif (dots[2][0] - dots[0][0]) / (dots[2][1] - dots[0][1]) == (dots[3][0] - dots[1][0]) / (dots[3][1] - dots[1][1]):
         answer = 1
+        
+    #3번 case
     elif (dots[3][0] - dots[0][0]) / (dots[3][1] - dots[0][1]) == (dots[2][0] - dots[1][0]) / (dots[2][1] - dots[1][1]):
         answer = 1
 
-        
     return answer
