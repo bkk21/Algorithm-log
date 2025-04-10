c43d5382c0dc3d72b3978821e571337e837556ff
Booooo Gyeong <108513540+bkk21@users.noreply.github.com>
2025-04-10 17:44:55 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -1,20 +1,10 @@
 def solution(common):
     answer = 0
     
-    try:
-        if common[1] / common[0] == common[2] / common[1]:
-            answer = common[-1] * (common[1] / common[0])
-        else:
-            answer = common[-1] + (common[1] - common[0])
-    except:
+    if common[0] != 0 and common[1] != 0 and common[1] / common[0] == common[2] / common[1]:
+        answer = common[-1] * (common[1] / common[0])
+    else:
         answer = common[-1] + (common[1] - common[0])
-    
-#     #등비
-#     if common[1] % common[0] == 0 and common[2] % common[1] == 0:
-#         answer = fini * (common[1] // common[0])
-#     else:
-#         answer = fini + (common[1] - common[0])
-        
-#         #print(answer)
+
         
     return answer
